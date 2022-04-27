43b59065f65d8b79494bdf9050f60551937dd49a
Gustavo Ferreira <gustavodelferreira.12@gmail.com>
2022-04-27 18:37:27 -0300

    test commit

---
+++ src/components/testimonials/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Container from './styles'
+
+const Testimonials = () => {
+  return (
+    <Container>
+      <div></div>
+    </Container>
+  )
+}
+
+export default Testimonials
